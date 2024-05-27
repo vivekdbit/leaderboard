6ec81f2ef298d191b9973e275c556ff279efe5e9
Vivek <vivek.raskar@kissht.com>
2024-05-27 16:47:07 -0700

    kinesis event added

--- app/routes.py
+++ app/routes.py
@@ -65,29 +65,30 @@ def upsert_score():
     # Initialize Boto3 client for Kinesis
     kinesis_client = boto3.client(
         'kinesis',
-        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
-        region_name=os.environ.get('AWS_REGION')
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        region_name=os.getenv('AWS_REGION')
     )
-
-    # Environment variable for Kinesis stream name
-    KINESIS_STREAM_NAME = os.environ.get('KINESIS_STREAM_NAME')
-
-    # Kinesis payload
-    kinesis_data = {
-        "request_identifier": request_identifier,
-        "user_id": data["user_id"],
-        "score": data["score"],
-        "event_time": datetime.datetime.utcnow().isoformat()
-    }
 
     # Put record to Kinesis stream
     try:
+        # Kinesis payload
+        kinesis_data = {
+            "request_identifier": request_identifier,
+            "user_id": data["user_id"],
+            "score": data["score"],
+            "event_time": datetime.datetime.utcnow().isoformat()
+        }
         kinesis_client.put_record(
-            StreamName=KINESIS_STREAM_NAME,
+            StreamName=os.environ.get('KINESIS_STREAM_NAME'),
             Data=json.dumps(kinesis_data),
             PartitionKey=data["user_id"]
         )
+        return jsonify({
+            "request-identifier": request_identifier,
+            "message": None,
+            "data": None
+        }), 200
     except Exception as e:
         print(f"Failed to send data to Kinesis: {str(e)}")
         return jsonify({
@@ -95,12 +96,6 @@ def upsert_score():
             "message": "Failed to send data to Kinesis",
             "data": None
         }), 500
-
-    return jsonify({
-        "request-identifier": request_identifier,
-        "message": None,
-        "data": None
-    }), 200
 
 
 @main.route('/users/calculate_winner', methods=['GET'])
